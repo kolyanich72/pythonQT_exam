f32defe46828c8c91fcd8a18c4e7fd1c9af1c876
kolyanich72 <127225551+kolyanich72@users.noreply.github.com>
2023-12-14 19:22:19 +0300

    Update main.py

--- script/exam_sys_util/main.py
+++ script/exam_sys_util/main.py
@@ -1,8 +1,9 @@
 from PySide6 import QtCore, QtWidgets, QtGui
-from common_info import  Common_info_window #, cur_proc, syst_info
+from common_info import Common_info_window  # , cur_proc, syst_info
 from cur_proc import Window as Wind_proc
 from net_info import Net_info_window
 import wmi, time
+
 
 class Window(QtWidgets.QWidget):
 
@@ -17,7 +18,6 @@ class Window(QtWidgets.QWidget):
         self.initSignal()
 
     def initSignal(self):
-
         self._common_info.close_but.clicked.connect(self._close_but_clicked)
         self.delay_spinBox.valueChanged.connect(self.setDelay)
 
@@ -38,14 +38,11 @@ class Window(QtWidgets.QWidget):
         self.tabWidget.setStyleSheet("background-color: lightgray")
         self.tabWidget.setMinimumWidth(600)
 
-
         self.delay_Label = QtWidgets.QLabel("таймер обновления")
         self.delay_spinBox = QtWidgets.QSpinBox()
 
-
         self.comn_info_label = QtWidgets.QVBoxLayout()
         self.comn_info_label.addLayout(self._common_info.main_layot)
-
 
         self.proc_info_label = QtWidgets.QPlainTextEdit('запущенные процессы и службы')
         self.proc_info_label.setReadOnly(True)
@@ -62,22 +59,18 @@ class Window(QtWidgets.QWidget):
 
         self.tabWidget.addTab(self.net, 'сеть')
 
-
         self.delay_spinBox.setMinimum(5)
 
         delay_Layout.addWidget(self.delay_Label)
         delay_Layout.addWidget(self.delay_spinBox)
-
 
         self.main_layout.addLayout(delay_Layout)
         self.main_layout.addWidget(self.tabWidget)
         self.main_layout.addWidget(self._common_info.close_but)
         self.setLayout(self.main_layout)
 
+    #   self.setLayout(self._common_info.main_layot)
 
-
-
-     #   self.setLayout(self._common_info.main_layot)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication()
